89bd7c8abf3971ea698c920b7d117f8be18c1717
Nils W <Nils W>
2017-03-16 20:00:36 +0100

    Added PAC3 restriction plugin

---
+++ lua/ev_plugins/sh_pac3.lua
@@ -0,0 +1,28 @@
+--[[-------------------------------------------------------------------------------------------------------------------------
+	Restricts PAC3 to specific ranks
+-------------------------------------------------------------------------------------------------------------------------]]--
+
+local PLUGIN = {}
+PLUGIN.Title = "PAC3"
+PLUGIN.Description = "Restricts PAC3."
+PLUGIN.Author = "MechWipf"
+PLUGIN.Privileges = { "PAC3", "PAC3_Editor" }
+
+function PLUGIN:Initialize()
+  hook.Add( "PrePACConfigApply", "EV_PAC3_Restriction", function ( ply )
+    if not ply:EV_HasPrivilege( "PAC3" ) then
+      evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
+      return false, "[EV] Insufficiant rank to use PAC3."
+    end
+  end )
+
+  hook.Add( "PrePACEditorOpen", "EV_PAC3_Restriction", function ( ply )
+    if not ply:EV_HasPrivilege( "PAC3_Editor" ) then
+      evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
+      return false, "[EV] Insufficiant rank to use PAC3."
+    end
+  end )
+end
+
+PLUGIN:Initialize()
+evolve:RegisterPlugin( PLUGIN )
